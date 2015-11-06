ed86fd5ef6022f79ce063d1ff5cacc32bf090ac7
peppepetra <giuseppex.petralia@intel.com>
2015-11-06 15:43:20 +0000

    Deserializer implementation
    HelloInput message
    EchoInput message
    EchoReplyInput message
    
    Change-Id: I2c33fb5b2ef90c42164b3329260d7e83bcbb95e5
    Signed-off-by: peppepetra <giuseppex.petralia@intel.com>

---
+++ openflowjava-extension/src/main/java/org/opendaylight/netide/openflowjava/protocol/impl/deserialization/factories/EchoInputMessageFactory.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 NetIDE Consortium and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerRegistry;
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerRegistryInjector;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInputBuilder;
+/**
+ * @author giuseppex.petralia@intel.com
+ *
+ */
+public class EchoInputMessageFactory implements OFDeserializer<EchoInput>, DeserializerRegistryInjector{
+
+    private DeserializerRegistry registry;
+    
+    @Override
+    public void injectDeserializerRegistry(DeserializerRegistry deserializerRegistry) {
+        registry = deserializerRegistry;
+    }
+    
+    @Override
+    public EchoInput deserialize(ByteBuf rawMessage) {
+        EchoInputBuilder builder = new EchoInputBuilder();
+        builder.setVersion((short) EncodeConstants.OF13_VERSION_ID);
+        builder.setXid(rawMessage.readUnsignedInt());
+        int remainingBytes = rawMessage.readableBytes();
+        if (remainingBytes > 0) {
+            builder.setData(rawMessage.readBytes(remainingBytes).array());
+        }
+        return builder.build();
+    }
+
+}
