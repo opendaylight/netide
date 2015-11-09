6b41d33fbf180418fa17400e60f826d58a6e4225
peppepetra <giuseppex.petralia@intel.com>
2015-11-09 12:07:22 +0000

    Removed useless deserializers.
    Echo and Hello messages.
    
    Change-Id: I6b6126daedd307628024a4ab3a0daf2a5151e1ac
    Signed-off-by: peppepetra <giuseppex.petralia@intel.com>

--- openflowjava-extension/src/main/java/org/opendaylight/netide/openflowjava/protocol/impl/deserialization/NetIdeMessageDeserializerInitializer.java
+++ openflowjava-extension/src/main/java/org/opendaylight/netide/openflowjava/protocol/impl/deserialization/NetIdeMessageDeserializerInitializer.java
@@ -8,15 +8,12 @@
 package org.opendaylight.netide.openflowjava.protocol.impl.deserialization;
 
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.BarrierInputMessageFactory;
-import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.EchoInputMessageFactory;
-import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.EchoReplyInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.FlowModInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.GetAsyncRequestMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.GetConfigInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.GetFeaturesInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.GetQueueConfigInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.GroupModInputMessageFactory;
-import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.HelloInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.MeterModInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.MultipartRequestInputMessageFactory;
 import org.opendaylight.netide.openflowjava.protocol.impl.deserialization.factories.PacketOutInputMessageFactory;
@@ -29,15 +26,12 @@ import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerRegi
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.protocol.impl.util.SimpleDeserializerRegistryHelper;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoReplyInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FlowModInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetAsyncInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetConfigInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetFeaturesInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetQueueConfigInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GroupModInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.HelloInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MeterModInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartRequestInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PacketOutInput;
@@ -57,9 +51,6 @@ public final class NetIdeMessageDeserializerInitializer {
         // register OF v1.3 NEW message deserializers
         SimpleDeserializerRegistryHelper helper;
         helper = new SimpleDeserializerRegistryHelper(EncodeConstants.OF13_VERSION_ID, registry);
-        //helper.registerDeserializer(0, null, HelloInput.class, new HelloInputMessageFactory());
-        //helper.registerDeserializer(2, null, EchoInput.class, new EchoInputMessageFactory());
-        //helper.registerDeserializer(3, null, EchoReplyInput.class, new EchoReplyInputMessageFactory());
         helper.registerDeserializer(5, null, GetFeaturesInput.class, new GetFeaturesInputMessageFactory());
         helper.registerDeserializer(7, null, GetConfigInput.class, new GetConfigInputMessageFactory());
         helper.registerDeserializer(9, null, SetConfigInput.class, new SetConfigInputMessageFactory());
